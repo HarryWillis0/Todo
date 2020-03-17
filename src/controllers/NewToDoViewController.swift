1b448d9fe703b1dbf4a418990f426600f25963d6
Harry <harry.willis0@gmail.com>
2020-03-17 16:02:19 -0600

    added controllers for entry table view, and adding new todo

---
+++ src/controllers/NewToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewToDoViewController.swift
+//  ToDo
+//
+//  Created by Harry George Willis on 2020-03-17.
+//  Copyright © 2020 harry. All rights reserved.
+//
+
+import UIKit
+
+class NewToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
