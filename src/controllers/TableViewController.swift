5aae13e6ea9facd4c4dc720ed2649e62e163c5ee
Harry <harry.willis0@gmail.com>
2020-03-25 10:03:59 -0600

    configure list cell

--- src/controllers/TableViewController.swift
+++ src/controllers/TableViewController.swift
@@ -1,47 +1,46 @@
 //
-//  ToDosTableViewController.swift
+//  TableViewController.swift
 //  ToDo
 //
-//  Created by Harry George Willis on 2020-03-17.
+//  Created by Harry George Willis on 2020-03-25.
 //  Copyright © 2020 harry. All rights reserved.
 //
 
 import UIKit
 
-class ToDosTableViewController: UITableViewController {
+class TableViewController: UITableViewController {
 
-    var lists: [List] = [List]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // retrieve lists from db
-        lists = ListOps.retrieveLists()!
-        
-        // edit button in navbar
-        self.navigationItem.rightBarButtonItem = self.editButtonItem
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lists.count
+        // #warning Incomplete implementation, return the number of rows
+        return 0
     }
 
-
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = lists[indexPath.row].title
 
         return cell
     }
-    
+    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -78,11 +77,14 @@ class ToDosTableViewController: UITableViewController {
     }
     */
 
+    /*
     // MARK: - Navigation
-    
-    // handle return segues from cancel button
-    @IBAction func cancel(segue: UIStoryboardSegue){
-        // do nothing
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
